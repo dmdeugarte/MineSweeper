2237478bff111a18632ba35b70a34b90756114aa
dmdeugarte <39715792+dmdeugarte@users.noreply.github.com>
2019-08-08 00:19:03 -0500

    Create Board.java

---
+++ Board.java
@@ -0,0 +1,96 @@
+import java.lang.Math;
+public class Board 
+{
+	private Tile[][] arr;
+	private int r;
+	private int c;
+	
+	public Board(int a, int b)
+	{
+		arr = new Tile[a][b];
+		r = a;
+		c = b;
+	}
+	
+	public void populateField(int n)	//randomly assigns 'n' mines to the array. Designated as 9.
+	{
+		int i = 0;
+		while(i < n)
+		{
+			int a = (int)(Math.random()*r);
+			int b = (int)(Math.random()*c);
+			
+			if(arr[a][b].getNum() != 9)
+			{
+				arr[a][b].setNum(9);
+				i++;
+			}
+				
+		}
+	}
+	
+	public void countMines()
+	{
+		for(int j = 0; j < r; j++)
+		{
+			for(int k = 0; k < c; k++)
+			{
+				if (arr[j][k].getNum() != 9)
+				{
+					int count = 0;
+					
+					if((j-1 >= 0 && k-1 >= 0) && arr[j-1][k-1].getNum() == 9)
+						count++;
+					
+					if((k-1 >= 0) && arr[j][k-1].getNum() == 9)
+						count++;
+				
+					if((j+1 < r && k-1 >= 0) && arr[j+1][k-1].getNum() == 9)
+						count++;
+				
+					if((j-1 >= 0) && arr[j-1][k].getNum() == 9)
+						count++;
+				
+					if((j+1 < r) && arr[j+1][k].getNum() == 9)
+						count++;
+				
+					if((j-1 >= 0 && k+1 < c) && arr[j-1][k+1].getNum() == 9)
+						count++;
+				
+					if((k+1 < c) && arr[j][k+1].getNum() == 9)
+						count++;
+				
+					if((j+1 < r && k+1 < c) && arr[j+1][k+1].getNum() == 9)
+						count++;
+					
+					arr[j][k].setNum(count);
+				}
+			}
+		}
+	}
+	
+	public void setMineStatus(int a, int b, boolean k)
+	{
+		if ((a>=0 && a<r) && (b>=0 && b<c))
+		{
+			arr[a][b].changeStatus(k);
+		}
+	}
+	
+	public void printField()
+	{
+		for(Tile[] r: arr)
+		{
+			for(Tile c: r)
+			{
+				if(c.isMarked())
+					System.out.print("x ");
+				else
+					System.out.print(c.getNum() + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	
+}
