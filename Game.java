26498d2e1ad4419f35d58b015afd16e1c58b4504
dmdeugarte <39715792+dmdeugarte@users.noreply.github.com>
2019-08-16 19:03:45 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,8 +1,12 @@
-
+import java.util.Scanner;
+import java.lang.String;
 public class Game 
 {
 	public static void main(String[] args)
 	{
+		Scanner scanNum = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
+		
 		Board game = new Board(9, 9);
 		
 		game.populateField(15);
@@ -14,5 +18,43 @@ public class Game
 		game.setMineStatus(1, 1, true);
 		
 		game.printField();
+		
+		boolean cont = true;
+		while(cont)
+		{
+			game.printField();
+			printMenu();
+			int choice = scanNum.nextInt();
+			
+			System.out.println("Coordinates?");
+			String coords = scan.nextLine();
+			
+			int a = Integer.parseInt(coords.substring(0, coords.indexOf("_")));
+			int b = Integer.parseInt(coords.substring(coords.indexOf("_")+1));
+			
+			switch(choice)
+			{
+			case 1: //game.revealTile(a, b);
+				break;
+			case 2: game.setMineStatus(a, b, true);
+				break;
+			case 9: //game.quitReveal();
+				break;
+			}
+				
+		}
+	}
+	
+	public static void printMenu()
+	{
+		System.out.println("Please choose a move.");
+		System.out.println("1) Reveal a Tile");
+		System.out.println("2) Mark a Tile");
+		System.out.println("9) Quit and Reveal");
+	}
+	
+	public static void printInstructions()	//separating so, if necessary, I can easily add, change, or remove instructions
+	{
+		System.out.println("Please note:\nWhen inputting coordinates, please input one coord, a space and a second coord.\n ie. 12_6");
 	}
 }
