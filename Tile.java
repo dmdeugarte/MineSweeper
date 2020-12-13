0a6ba4bc8cacae252d74870acb89ef438aa9dfa5
dmdeugarte <39715792+dmdeugarte@users.noreply.github.com>
2020-12-13 17:16:28 -0600

    Update Tile.java

--- Tile.java
+++ Tile.java
@@ -12,31 +12,54 @@ public class Tile
 		isSeen = inSeen;
 	}
 	
+	/**
+	 * Changes the isMarked value to the parameter provided
+	 * @param k a boolean to change the isMarked value to
+	 */
 	public void changeStatus(boolean k)
 	{
 		isMarked = k;
 	}
 	
+	/**
+	 * Returns the number value of the tile 0-9
+	 * @return the number value of the tile
+	 */
 	public int getNum()
 	{
 		return num;
 	}
 	
+	/**
+	 * Returns the isMarked value of the tile
+	 * @return the isMarked value of the tile
+	 */
 	public boolean isMarked()
 	{
 		return isMarked;
 	}
 	
+	/**
+	 * Changes the number value of the tile.
+	 * @param n the number value to change the tile to
+	 */
 	public void setNum(int n)
 	{
 		num = n;
 	}
 	
+	/**
+	 * Returns the isSeen value of the tile
+	 * @return returns the isSeen value of the tile. True for is visible and false for not visible
+	 */
 	public boolean isSeen()
 	{
 		return isSeen;
 	}
 	
+	/**
+	 * Sets the isSeen value to seen. There is no reversing this.
+	 */
 	public void setSeen()
 	{
 		isSeen = true;
