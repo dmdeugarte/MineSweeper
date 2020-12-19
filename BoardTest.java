f7b891e367b8a4fba1114b85b1b37d2744bd4720
dmdeugarte <39715792+dmdeugarte@users.noreply.github.com>
2020-12-18 18:54:03 -0600

    Update BoardTest.java

--- BoardTest.java
+++ BoardTest.java
@@ -5,6 +5,7 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.lang.Math;
 
 public class BoardTest extends JFrame //implements ActionListener
 {
@@ -37,14 +38,17 @@ public class BoardTest extends JFrame //implements ActionListener
         boardLength = 10;
         boardMines = 15;
         
-        /*tileArray = new Tile[boardHeight][boardLength];
+        tileArray = new Tile[boardHeight][boardLength];
         for(int j = 0; j < boardHeight; j++)
         {
             for(int k = 0; k < boardLength; k++)
             {
                 tileArray[j][k] = new Tile(0, false, false);
             }
-        }*/
+        }
+        
+        this.populateField();
+        this.countMines();
         
         //Stuff not done above the compiler. Built from the Default values.
         buttons = new JButton[boardHeight * boardLength];
@@ -88,6 +92,11 @@ public class BoardTest extends JFrame //implements ActionListener
         setVisible(true);
     }
     
+    /**
+     * Creates an ActionListener for a button in the Board's grid
+     * @param tileNum The button's bucket number
+     * @return the ActionListener in question.
+     */
     public static ActionListener tileClickedListener(final int tileNum)
     {
         return new
@@ -99,25 +108,153 @@ public class BoardTest extends JFrame //implements ActionListener
                         if (markOrReveal.getText().equals("REVEALING"))
                             markMode = false;
                         
-                        if(markMode && buttons[tileNum].getText().equals("" + HIDDEN))
+                        int x = rowFrom1D(tileNum);
+                        int y = colFrom1D(tileNum);
+                        
+                        if(markMode)
                         {
                             if (buttons[tileNum].getText().equals("" + HIDDEN))
+                            {
                                 buttons[tileNum].setText("" + MARKED);
+                                tileArray[x][y].changeStatus(true);
+                            }
                             else if (buttons[tileNum].getText().equals("" + MARKED))
+                            {
                                 buttons[tileNum].setText("" + HIDDEN);
+                                tileArray[x][y].changeStatus(false);
+                            }
                         }
                         else if (!markMode && buttons[tileNum].getText().equals("" + HIDDEN))
                         {
-                            buttons[tileNum].setText("Num");
+                            buttons[tileNum].setText("" + tileArray[x][y].getNum());
+                            if (revealTile(x, y))
+                                quitReveal();
                         }
                     }
                 };
     }
     
-    /*private int posFrom2D(int row, int col)
+    /**
+     * Randomly populates a game field with mines.
+     * @param n the number of mines to be populated
+     */
+    public void populateField()    //randomly assigns 'n' mines to the array. Designated as 9.
+    {
+        int i = 0;
+        while(i < boardMines)
+        {
+            int a = (int)(Math.random()*boardHeight);
+            int b = (int)(Math.random()*boardLength);
+            
+            if(tileArray[a][b].getNum() != 9)
+            {
+                tileArray[a][b].setNum(9);
+                i++;
+            }
+        }
+    }
+    
+    /**
+     * Assigns values to tiles that are not mines based on the number of mines in their peripherals. 
+     */
+    public void countMines()
+    {
+        for(int j = 0; j < boardHeight; j++)
+        {
+            for(int k = 0; k < boardLength; k++)
+            {
+                if (tileArray[j][k].getNum() != 9)
+                {
+                    int count = 0;
+                    
+                    if((j-1 >= 0 && k-1 >= 0) && tileArray[j-1][k-1].getNum() == 9)
+                        count++;
+                    
+                    if((k-1 >= 0) && tileArray[j][k-1].getNum() == 9)
+                        count++;
+                
+                    if((j+1 < boardHeight && k-1 >= 0) && tileArray[j+1][k-1].getNum() == 9)
+                        count++;
+                
+                    if((j-1 >= 0) && tileArray[j-1][k].getNum() == 9)
+                        count++;
+                
+                    if((j+1 < boardHeight) && tileArray[j+1][k].getNum() == 9)
+                        count++;
+                
+                    if((j-1 >= 0 && k+1 < boardLength) && tileArray[j-1][k+1].getNum() == 9)
+                        count++;
+                
+                    if((k+1 < boardLength) && tileArray[j][k+1].getNum() == 9)
+                        count++;
+                
+                    if((j+1 < boardHeight && k+1 < boardLength) && tileArray[j+1][k+1].getNum() == 9)
+                        count++;
+                    
+                    tileArray[j][k].setNum(count);
+                }
+            }
+        }
+    }
+    
+    /**
+     * Changes the isSeen value of a tile. Determines if a game is lost. 
+     * @param a The row value of the coordinate
+     * @param b The column value of the coordinate
+     * @return true if a mine was revealed
+     *         false is a regular tile was revealed
+     */
+    public static boolean revealTile(int a, int b) //will return a boolean value of if a mine was revealed and one should quit the game. 
+    {
+        if (tileArray[a][b].getNum() == 9)
+            return true;
+        else 
+        {
+            tileArray[a][b].setSeen();
+            return false;
+        }
+    }
+    
+    /**
+     * Ends a game by revealing every tile.
+     */
+    public static void quitReveal()
+    {
+        for(int j = 0; j < boardHeight; j++)
+        {
+            for(int k = 0; k < boardLength; k++)
+            {
+                tileArray[j][k].setSeen();
+                buttons[posFrom2D(j,k)].setText("" + tileArray[j][k].getNum());
+            }
+        }
+    }
+    
+    /**
+     * Starts a game by revealing a random tile with no mines near it.
+     */
+    public void startGame()
+    {
+        boolean cont = true;
+        
+        while(cont)
+        {
+            int a = (int)(Math.random()*boardHeight);
+            int b = (int)(Math.random()*boardLength);
+            
+            if(tileArray[a][b].getNum() == 0)
+            {
+                tileArray[a][b].setSeen();
+                buttons[posFrom2D(a,b)].setText("" + tileArray[a][b].getNum());
+                cont = false;
+            }
+        }
+    }
+    
+    private static int posFrom2D(int row, int col)
     {
         return row*boardLength + col;
-    }*/
+    }
     
     private static int rowFrom1D(int pos)
     {
@@ -133,6 +270,8 @@ public class BoardTest extends JFrame //implements ActionListener
     {
         // TODO Auto-generated method stub
         BoardTest b = new BoardTest();
+        
+        b.startGame();
     }
 
 }
